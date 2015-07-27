a545222ca96c32cccf429f9929b99f757f31f2e8
Amber Hornsby <AmberHornsby@asosxlap113.nat.physics.ox.ac.uk>
2015-07-27 15:01:01 +0100

    updated - actually works now (I think)

--- bulgeless_galaxies.py
+++ bulgeless_galaxies.py
@@ -20,20 +20,50 @@ def green_valley(Mr):
 def red(Mr,colour):
     return colour > green_valley(Mr)
 #true false array
-def TF_array(R,galaxy,bins_array):
+def TF_array(data,column,bins_array):
     c = []
     for i in xrange(bins_array.shape[0]):
         #digitize calculates hist bins, in1d identifies where two arrays have 
         #same value
-        z = np.where(np.in1d(bins_array[np.digitize(R[galaxy],bins_array)],
-                               bins_array[i]))
+        z = np.where(np.in1d(bins_array[np.digitize(data[...,column],
+                                                    bins_array)],
+                                                    bins_array[i]))
         c.append(z[0])
     return np.asarray(c)
+def index(bin1,galaxy,a,b,c,d,e,f,g,h,k,l,m,r_band,u_band,NUV,NUV_err,u_err,r_err,
+          dr8,ra,dec,redshift,fraction):
+    for i in xrange(len(bin1)):
+        for j in (np.asarray(bin1)[i]):
+            a.append(galaxy[j,r_band])
+            b.append(galaxy[j,u_band]-galaxy[j,r_band])
+            c.append(-1*((-1*galaxy[j,NUV])-galaxy[j,u_band]))
+            d.append(np.sqrt((galaxy[j,NUV_err])**2 + (galaxy[j,u_err])**2))
+            e.append(np.sqrt((galaxy[j,u_err])**2+(galaxy[j,r_err])**2))
+            f.append(galaxy[j,dr8])
+            g.append(galaxy[j,ra])
+            h.append(galaxy[j,dec])
+            k.append(galaxy[j,redshift]) 
+            l.append(galaxy[j,u_band])
+            m.append(galaxy[j,fraction])
+    bulgeless_final = np.column_stack((np.asarray(b), np.asarray(e),
+                                      np.asarray(c), np.asarray(d),
+                                      np.asarray(k), np.asarray(f),
+                                      np.asarray(g), np.asarray(h)))
+    red_new = np.asarray(a)
+    u_new = np.asarray(l)
+    fraction_new = np.asarray(m)
+    return fraction_new,u_new,red_new,bulgeless_final
 #Data Input####################################################################
 read_data = False
 if read_data:
     data = np.genfromtxt('combined.csv', delimiter = ',',skip_header = 1)
     np.save('data', data)
+    #importing NUV data from bulged/bulgeless galaxies
+    NUV_bulged = np.genfromtxt("bulged.csv", delimiter = ",", skiprows = 1)
+    np.save('bulged', NUV_bulged)
+    NUV_bulgeless = np.genfromtxt("bulgeless.csv", delimiter = ",",
+                                  skiprows = 1)
+    np.save('bulgeless', NUV_bulgeless)
 data = np.load('data.npy')
 '''
 Column 1 = dr8  ;   Column 3 = ra_1     ;   Column 4 = dec
@@ -43,21 +73,31 @@ Column 85 = dominant    ;   Column 278 = redshift   ;   Column 280 = U
 Column 281 = G  ;   Column 282 = R  ;   Column 285 = error in U
 Column 287 = error in R
 '''
-np.savetxt("cut.csv", data[0:500], delimiter = ",")
+#columns
+r_band = 282
+r_err = 287
+u_band = 280
+u_err = 285
+g_band = 281
+redshift = 278
+NUV = 304
+NUV_err = 305
+dr8 = 1
+ra = 3
+dec = 4
+bulgeless = 69
+dominant1 = 85
 #selecting data ###############################################################
 disk = data[...,21]
 face_on = data[...,43]
-no_bulge = data[...,69]
+no_bulge = data[...,bulgeless]
 just_noticeable = data[...,75]
 obvious = data[...,81]
-dominant = data[...,85]
-redshift = data[...,278]
-U = data[...,280]
-G = data[...,281]
-R = data[...,282]
-errU = data[...,285]
-errR = data[...,287]
-
+dominant = data[...,dominant1]
+U = data[...,u_band]
+G = data[...,g_band]
+R = data[...,r_band]
+redshift = data[...,redshift]
 #maths#########################################################################
 gminusr = G-R
 uminusr = U-R
@@ -153,8 +193,8 @@ print "number of red dominant galaxies = %.2d" % a
 print "total number of galaxies in red bulge/bulgeless catagory = %.2d" % tot
 #plotting magnitude histograms#################################################
 #importing NUV data from bulged/bulgeless galaxies
-#red_bulged = np.genfromtxt("bulged.csv", delimiter = ",", skiprows = 1)
-#red_bulgeless = np.genfromtxt("bulgeless.csv", delimiter = ",", skiprows = 1)
+NUV_bulged = np.load('bulged.npy')
+NUV_bulgeless =  np.load('bulgeless.npy')
 ###############################################################################
 bins = 24
 bins_array = np.linspace(-24,-12,num = 13)
@@ -166,9 +206,9 @@ plt.subplot(122)
 plt.hist(R[red_bulged], bins=bins, normed = True, label = "bulged")
 plt.legend(loc="best")
 #extracting bulgeless data from bins ##########################################
-TF_array_bulgeless = TF_array(R,red_bulgeless,bins_array)  
+TF_array_bulgeless = TF_array(NUV_bulgeless,282,bins_array)  
 #extracting bulged data from bins #############################################
-TF_array_bulged = TF_array(R,red_bulged, bins_array)
+TF_array_bulged = TF_array(NUV_bulged,282,bins_array)  
 a = []
 b = []
 for i in xrange(bins_array.shape[0]):
@@ -187,33 +227,78 @@ for i in xrange(bins_array.shape[0]):
                                          size = (bulged_size))
             a.append(bulged)
             b.append(bulgeless)    
-
+#organising indices############################################################
 n = []   
 m = []
-o = []   
+o = []  
+p = [] 
 q = []
-for i in xrange(len(b)):
-    for j in (np.asarray(b)[i]):
-        n.append(R[red_bulgeless][j])
-        m.append(uminusr[red_bulgeless][j])
-for i in xrange(len(a)):
-    for j in (np.asarray(a)[i]):
-        o.append(R[red_bulged][j])
-        q.append(uminusr[red_bulged][j])
-        
-redbulgeless = np.asarray(n)
-uminusr_bulgeless = np.asarray(m)
-redbulged = np.asarray(o)
-uminusr_bulged = np.asarray(q)
-
-plt.figure() #figure 4
+r = []
+s = []
+t = []
+u = []
+v = []
+w = []
+bulgeless,u_bulgeless,redbulgelessfinal, bulgeless_final = index(b,NUV_bulgeless,n,m,o,p,
+                                                       q,r,s,t,u,v,w,r_band,
+                                                       u_band,NUV,NUV_err,
+                                                       u_err,r_err,dr8,ra,dec,
+                                                       redshift,no_bulge)      
+#f = open('list_commands_bulgeless.txt', 'a')
+#b = bulgeless_final
+#for n in range(len(b)):
+#    f.write('python starpy.py '+str(b[n,0])+' '+str(b[n,1])+' '+str(b[n,2])+' '+str(b[n,3])+' '+str(b[n,4])+'  '+str(b[n,5].astype(int))+' '+str(b[n,6])+' '+str(b[n,7])+'\n')
+#f.close()                                                    
+n1 = []   
+m1 = []
+o1 = []  
+p1 = [] 
+q1 = []
+r1 = []
+s1 = []
+t1 = []
+u1 = []       
+v1 = []
+w1 = []
+                                
+bulged,u_bulged,redbulgedfinal, bulged_final = index(a,NUV_bulged,n1,m1,o1,p1,q1,r1,
+                                              s1,t1,u1,v1,w1,r_band,u_band,NUV,
+                                              NUV_err,u_err, r_err,dr8,ra,dec,
+                                              redshift,dominant)
+#f = open('list_commands_bulged.txt', 'a')
+#b = bulged_final
+#for n in range(len(b)):
+#    f.write('python starpy.py '+str(b[n,0])+' '+str(b[n,1])+' '+str(b[n,2])+' '+str(b[n,3])+' '+str(b[n,4])+'  '+str(b[n,5].astype(int))+' '+str(b[n,6])+' '+str(b[n,7])+'\n')
+#f.close()                                               
+#test plotting - R,U-R#########################################################
+plt.figure() #figure 4 U-R,R
 ax = plt.gca()
 ax.invert_xaxis()
 plt.xlim(-16,-25)
 plt.ylim(0.5,4)
-plt.plot(redbulged,uminusr_bulged, "ro", label = "bulged")
-plt.plot(redbulgeless,uminusr_bulgeless, "bo",label = "bulgeless")
+plt.plot(redbulgedfinal,bulged_final[...,0], "ro", label = "bulged")
+plt.plot(redbulgelessfinal,bulgeless_final[...,0], "bo",label = "bulgeless")
 plt.plot(g,gv,'g-', linewidth = 3, label = "green valley")
 plt.legend(loc="best")
-plt.xlabel("Absolute R")
+plt.xlabel("R")
+plt.ylabel("U - R")
+#test plotting - NUV-U,U-R#####################################################
+plt.figure() #figure 5 NUV-U,U-R
+ax = plt.gca()
+ax.invert_xaxis()
+plt.plot(bulged_final[...,0],bulged_final[...,2], "ro", label = "bulged")
+plt.plot(bulgeless_final[...,0],bulgeless_final[...,2], "bo",
+         label = "bulgeless")
+#plt.plot(g,gv,'g-', linewidth = 3, label = "green valley")
+plt.legend(loc="best")
+plt.ylabel("NUV-U")
+plt.xlabel("U - R")
+#test plotting - vote fraction vs redshift#####################################
+plt.figure() #figure 6 vote fraction, redshift
+ax = plt.gca()
+plt.plot(redbulgedfinal,bulged_final[...,0], "ro", label = "bulged")
+plt.plot(redbulgelessfinal,bulgeless_final[...,0], "bo",label = "bulgeless")
+plt.plot(g,gv,'g-', linewidth = 3, label = "green valley")
+plt.legend(loc="best")
+plt.xlabel("R")
 plt.ylabel("U - R")
